66bfe2536b2aaf6299fa8c45457b314bd6ad734b
Gonzalo Garcia <gonzalogacc@gmail.com>
2017-02-22 16:50:04 +0000

    TenX pather added to main contigger, test1

--- src/TenX/TenX_pather.cpp
+++ src/TenX/TenX_pather.cpp
@@ -67,7 +67,7 @@ int TenXPather::createEmptyMap(HyperBasevector* hbv){
   std::cout  << Date() << ": Creating the kmer:set pair to insert" << std::endl;
 //  for (auto e = 0; e<edges.size(); ++e) {
 #pragma omp parallel for
-  for (auto e=0; e<10000; ++e) { // TODO: [GONZA] fix this to run in a bigger machine, is like this for the map to fit in my laptop :/
+  for (auto e=0; e<50000; ++e) { // TODO: [GONZA] fix this to run in a bigger machine, is like this for the map to fit in my laptop :/
     auto seq = edges[e].ToString();
     if (seq.length()>min_edge_length){
       auto kv = ProduceKmers(seq);
--- src/modules/w2rap-contigger.cc
+++ src/modules/w2rap-contigger.cc
@@ -33,7 +33,7 @@
 //#include "pacbio/pacbio_pather.h"
 
 #include "TenX/TenX_pather.h"
-//#include "TenX/PathFinder_tx.h"
+#include "TenX/PathFinder_tx.h"
 
 std::string checkpoint_perf_time(const std::string section_name){
     static double wtimer, cputimer;
@@ -703,21 +703,26 @@ int main(const int argc, const char * argv[]) {
 //        TenXPather txp (&tx_reads, &hbvr);
         // TODO: check this bit
         auto edges = hbvr.Edges();
-        TenXPather txp (tx_reads, hbvr, inv, 5, edges, pathsr, paths_inv);
+
+        // TenX Test
+        auto tenx_reads = dataMag.mag["TEX"]->rReads;
+        std::cout << "Reads already loaded..." << std::endl;
+        std::cout << tenx_reads.size() << " Reads in the vector" << std::endl;
+
+        // Create the paths and invert them
+        std::cout << "Starting tenxPather..." << std::endl;
+        TenXPather txp (tenx_reads, hbvr, inv, 5, edges, pathsr, paths_inv);
 
         std::cout<< Date() << " Map creation." << std::endl;
         txp.createEmptyMap(&hbvr);
-        std::cout<< Date() << " Map creation done..." << std::endl;
+
         std::cout<< Date() << " Map filling with reads..." << std::endl;
         txp.reads2kmerTagMap();
-        std::cout<< Date() << " Map filling with reads done..." << std::endl;
-        txp.solve_region_using_TenX(5000, true);
 
-//        // execute pathfinder here
-//        std::cout<< Date() << " Starting pathfinder..." << std::endl;
-//        PathFinder_tx pf_tx (&txp, &hbvr, inv, 5);
-//        std::cout<< Date() << " done pathfinder..." << std::endl;
-//        pf_tx.solve_region_using_TenX(3000, true);
+        // Pathfinder
+        std::cout<< Date() << " Starting pathfinder..." << std::endl;
+        std::cout<< Date() << " done pathfinder..." << std::endl;
+        txp.solve_region_using_TenX(5000, true);
 
         RemoveUnneededVertices2(hbvr, inv, pathsr);
         Cleanup(hbvr, inv, pathsr);
