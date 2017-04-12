aa24bece9bd422f1d9e216e2334e03a55ab58aeb
Bernardo J. Clavijo <bjclavijo@gmail.com>
2017-04-12 11:57:02 +0100

    spring cleanup

--- src/paths/long/large/ExtractReads.cc
+++ src/paths/long/large/ExtractReads.cc
@@ -12,19 +12,13 @@
 #include <util/OutputLog.h>
 #include "Basevector.h"
 #include "Bitvector.h"
-#include "FastIfstream.h"
 #include "FetchReads.h"
 #include "PairsManager.h"
-#include "ParseSet.h"
-#include "Qualvector.h"
 #include "TokenizeString.h"
 #include "bam/ReadBAM.h"
-#include "feudal/ObjectManager.h"
-#include "feudal/PQVec.h"
 #include "math/HoInterval.h"
 #include "paths/long/LoadCorrectCore.h"
 #include "paths/long/large/ExtractReads.h"
-#include "paths/long/large/ReadNameLookup.h"
 
 void ExtractReads( String reads, const String& work_dir, vecbvec* pReads, VecPQVec* quals )
 {
