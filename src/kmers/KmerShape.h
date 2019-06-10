6a1995db261a6ca1108e7a476caa506aec9f19ef
Luis Yanes <yanes.luis@gmail.com>
2019-06-10 17:43:30 +0100

    Fix [osx,g++-8] compilation issue

--- src/kmers/KmerShape.h
+++ src/kmers/KmerShape.h
@@ -563,7 +563,7 @@ public:
 
  // Method: getId
  // Return a <KmerShapeId> object that uniquely identifies this particular kmer shape
- static const String getId() { return KmerShapeId(getStringId()); }
+ static const String getId() { return getStringId(); }
 
  private:
   // Method: getStringId
