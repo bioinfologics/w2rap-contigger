78e3934d844d4cef3b75fb76dee5dcd673aded19
Luis Yanes <yanes.luis@gmail.com>
2019-09-02 14:19:05 +0100

    Add binary to explore edges support

--- src/modules/edge_support.cc
+++ src/modules/edge_support.cc
@@ -3,12 +3,17 @@
 #include <paths/HyperBasevector.h>
 #include <paths/long/ReadPath.h>
 #include <util/OutputLog.h>
+#include <unordered_map>
 
-vec<int> get_supports(const HyperBasevector &hb, vec<int> &inv, ReadPathVec &paths) {
+vec<int> get_supports(const HyperBasevector &hb, vec<int> &inv, ReadPathVec &paths, std::unordered_map<int,std::vector<int>> &edges) {
     vec<int> support(hb.EdgeObjectCount(), 0);
     for (const auto & path : paths) {
         for (int64_t j = 0; j < (int64_t) path.size(); j++) {
             int e = path[j];
+            auto edge_lookup(edges.find(e));
+            if (edge_lookup != edges.cend()) {
+                edge_lookup->second.push_back(j);
+            }
             if (j >= 1) support[e]++;
             if (inv[e] >= 0 && j < (int64_t) path.size() - 1)
                 support[inv[e]]++;
@@ -19,7 +24,6 @@ vec<int> get_supports(const HyperBasevector &hb, vec<int> &inv, ReadPathVec &pat
 
 int main(int argc, char **argv) {
     std::cout << "Hello world" << std::endl;
-    std::vector<int> edges;
     HyperBasevector hbv;
     vec<int> hbvinv;
     ReadPathVec paths;
@@ -32,10 +36,18 @@ int main(int argc, char **argv) {
     //load paths
     OutputLog(2) <<"Loading paths..." << std::endl;
     LoadReadPathVec(paths,argv[2]);
-
-    auto supports=get_supports(hbv,hbvinv,paths);
+    std::unordered_map<int, std::vector<int>> edges;
     for (int i=3; i < argc; i++) {
-        std::cout << "edge" << argv[i] << ": " << supports[std::stoi(argv[i])] << std::endl;
+        edges[std::stoi(argv[i])];
+    }
+    auto supports=get_supports(hbv,hbvinv,paths,edges);
+
+    for (const auto &edge_paths : edges) {
+        std::cout << "edge"<<edge_paths.first<<":"<<std::endl;
+        for (const auto &path : edge_paths.second) {
+            std::cout << path << " ";
+        }
+        std::cout << std::endl;
     }
 
     return 0;
