27c3f94d82db449e1bad43cee176993a5a055a60
Luis Yanes <yanes.luis@gmail.com>
2019-09-02 14:40:11 +0100

    store path index

--- src/modules/edge_support.cc
+++ src/modules/edge_support.cc
@@ -5,19 +5,21 @@
 #include <util/OutputLog.h>
 #include <unordered_map>
 
-vec<int> get_supports(const HyperBasevector &hb, vec<int> &inv, ReadPathVec &paths, std::unordered_map<int,std::vector<int>> &edges) {
+vec<int> get_supports(const HyperBasevector &hb, vec<int> &inv, ReadPathVec &paths, std::unordered_map<int,std::vector<int64_t>> &edges) {
     vec<int> support(hb.EdgeObjectCount(), 0);
+    int64_t p(0);
     for (const auto & path : paths) {
         for (int64_t j = 0; j < (int64_t) path.size(); j++) {
             int e = path[j];
             auto edge_lookup(edges.find(e));
             if (edge_lookup != edges.cend()) {
-                edge_lookup->second.push_back(j);
+                edge_lookup->second.push_back(p);
             }
             if (j >= 1) support[e]++;
             if (inv[e] >= 0 && j < (int64_t) path.size() - 1)
                 support[inv[e]]++;
         }
+        p++;
     }
     return support;
 }
@@ -36,7 +38,7 @@ int main(int argc, char **argv) {
     //load paths
     OutputLog(2) <<"Loading paths..." << std::endl;
     LoadReadPathVec(paths,argv[2]);
-    std::unordered_map<int, std::vector<int>> edges;
+    std::unordered_map<int, std::vector<int64_t>> edges;
     for (int i=3; i < argc; i++) {
         edges[std::stoi(argv[i])];
     }
