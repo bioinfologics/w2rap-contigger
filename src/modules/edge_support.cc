348fda178d90568cbd07bdbc4daf8d110d52e4f5
Luis Yanes <yanes.luis@gmail.com>
2019-09-03 12:02:16 +0100

    Print the nts of the reads supporting the edge

--- src/modules/edge_support.cc
+++ src/modules/edge_support.cc
@@ -33,18 +33,25 @@ vec<int> get_supports(const HyperBasevector &hb, vec<int> &inv, ReadPathVec &pat
 
 int main(int argc, char **argv) {
     std::cout << "Hello world" << std::endl;
-    HyperBasevector hbv;
-    vec<int> hbvinv;
-    ReadPathVec paths;
 
+    // Load graph
+    HyperBasevector hbv;
     BinaryReader::readFile(argv[1], &hbv);
+
     //Create inversion
     OutputLog(4) <<"Creating graph involution..." << std::endl;
-    hbvinv.clear();
+    vec<int> hbvinv;
     hbv.Involution(hbvinv);
+
     //load paths
     OutputLog(2) <<"Loading paths..." << std::endl;
+    ReadPathVec paths;
     LoadReadPathVec(paths,argv[2]);
+
+    // Load reads
+    vecbvec bases;
+    bases.ReadAll(argv[3]);
+
     std::unordered_map<int, std::vector<int64_t>> edges;
     for (int i=3; i < argc; i++) {
         edges[std::stoi(argv[i])];
@@ -54,7 +61,7 @@ int main(int argc, char **argv) {
     for (const auto &edge_paths : edges) {
         std::cout << "edge"<<edge_paths.first<<": " << supports[edge_paths.first] << " paths support this edge"<<std::endl;
         for (const auto &path : edge_paths.second) {
-            std::cout << path << " ";
+            std::cout << path << bases[path].ToString() << "\n";
         }
         std::cout << std::endl;
     }
